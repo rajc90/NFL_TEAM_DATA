1fb97f3891b328466778ddfb4449f29b8aa502e9
rajc90 <rchaphekar@live.com>
2023-03-12 12:10:52 -0700

    Script to Clean W/L data

--- win-loss-clean.py
+++ win-loss-clean.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+#Win_loss_data Cleaning
 win_loss_data = pd.read_csv("C:/Users/rchap/Git/NFL_TEAM_DATA/CSV_Data/Win_Loss.csv")
 win_loss_data[['Wins', 'Losses', 'Ties']] = win_loss_data['Win-Loss'].apply(lambda x: pd.Series(str(x).split('-')))
 win_loss_data['Win%'] = win_loss_data['Win%'].apply(lambda x: pd.Series(str(x).replace('%','')))
